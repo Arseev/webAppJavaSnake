3c8e425edf3af88ed5366676c32de17ba6b54aa0
Arseev <arseevers@gmail.com>
2017-02-25 13:44:47 -0500

    Uploading Snake java game.

---
+++ sketch.js
@@ -0,0 +1,95 @@
+// Web Application Development
+// Final Project
+// Adam Seevers, Sam Uribe
+
+//snake variable
+var s;
+//check the game ending parameters
+var endGame;
+//size of the width of columns and rows used for placing block and snake movement
+var scl = 20;
+//increase in snake length as it gets a block
+var lUp;
+
+//initialize the game board and the snake object and the block object
+function setup() {
+  createCanvas(600 ,600);
+  keyCode = RIGHT_ARROW;
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+}
+
+function newGame() {
+  background(255,0,0);
+  keyCode = RIGHT_ARROW;
+  s = new Snake();
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  lUp = createVector(floor(random(cols)), floor(random(rows)));
+  lUp.mult(scl);
+}
+
+// draws the current snake onto the canvas of the html web page.
+function draw() {
+  if (s.tail.length > 5) {
+    background(0);
+  } else {
+    background(165);
+  }
+  if (s.x == 0 && keyCode === LEFT_ARROW) {
+     snake_die();
+  }
+  else if(s.y >= 580 && keyCode === DOWN_ARROW) {
+     snake_die();
+  }
+  else if(s.y == 0 && keyCode === UP_ARROW) {
+     snake_die();
+  }
+  else if(s.x >= 580 && keyCode === RIGHT_ARROW) {
+     snake_die();
+  }
+
+  s.update();
+  s.show();
+
+//If snake eats a block, rendomly place a new block
+  if (s.eat(lUp)) {
+    pickLocation();
+  }
+
+  fill(0,0,255);
+  rect(lUp.x, lUp.y, scl, scl);
+
+}
+
+
+// Monitors directional input for snake from the user via keyboard
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    if (s.yspeed !== 1)
+      s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    if (s.yspeed !== -1)
+      s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    if (s.xspeed !== -1)
+      s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    if (s.xspeed !== 1)
+      s.dir(-1, 0);
+  }
+}
+
+//Parameters for the snake losing
+function snake_die() {
+  background(255,0,0);
+}
+
+
+
+
+
---
+++ snake.js
@@ -0,0 +1,66 @@
+// Web Application Development
+// Final Project
+// Adam Seevers, Sam Uribe
+// This file contains the snake object and all attributes associated with it.
+
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 1;
+  this.tail = [];
+
+  // Check to see if the snake has met a block to eat and increase snake length
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d >= 1) {
+      return false;
+    } else {
+      this.total++;
+      return true;
+    }
+  }
+
+  this.dir = function(x , y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.update = function() {
+
+    if (this.total !== this.tail.length) {
+      for (var i = 0; i < this.tail.length-1; i++) {
+        this.tail[i] = this.tail[i+1];
+      }
+    }
+
+    this.tail[this.total-1] = createVector(this.x, this.y);
+
+    for (var j = 0; j < this.total-1; j++) {
+      this.tail[j] = this.tail[j+1];
+    }
+    this.tail[this.total-1] = createVector(this.x,this.y);
+
+
+    this.x = this.x + this.xspeed*scl;
+    this.y = this.y + this.yspeed*scl;
+
+    this.x = constrain(this.x, 0, width-scl);
+    this.y = constrain(this.y, 0, height-scl);
+
+
+  }
+
+  // displays the snake as it increases in size
+  this.show = function() {
+    fill(0,150,0);
+    for (var i = 0; i < this.tail.length-1; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl)
+    }
+    rect(this.x, this.y, scl, scl);
+
+  }
+
+  //snake dies when it hits a wall or itself.
+}
